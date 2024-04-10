1522eb99accb2097a4db342d7547b8a9a4f2ddc6
SimKennedy <dev@simkennedy.com>
2024-04-10 13:54:35 +0200

    fix: update settings endpoint URLs to use v2 endpoints

--- hugie/settings.py
+++ hugie/settings.py
@@ -7,6 +7,9 @@ class Settings(BaseModel):
     """Core settings."""
 
     base_url: str = "https://api.endpoints.huggingface.cloud"
-    endpoint_url: str = f"{base_url}/endpoint"
-    provider_url: str = f"{base_url}/provider"
+    # endpoint_url: str = f"{base_url}/endpoint"
+    # provider_url: str = f"{base_url}/provider"
+    namespace: str = os.getenv("HUGGINGFACE_NAMESPACE", "oxfordmedicalsimulation")
+    endpoint_url: str = f"{base_url}/v2/endpoint/{namespace}"
+    provider_url: str = f"{base_url}/v2/provider/{namespace}"
     token: str = os.getenv("HUGGINGFACE_READ_TOKEN")
